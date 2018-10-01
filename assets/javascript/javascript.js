46d2f679c1ed7fd5352fe599ce40be64384721c6
Michael Pohlman <michael.pohlman1@gmail.com>
2018-10-01 16:52:47 -0400

    updated folders and files for organization

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,26 @@
+// loop
+
+// select randam variable from list or words;
+
+// 3 Capture user key presses
+
+// 3a keyup function will start. Compare userinput to wrong guessbox inputs; return true if user input is in wrong guessbox; return false if not in the user guessbox
+
+// 3b If statement is true, ignore keypress
+
+// 3c if statement is false go to step 4
+
+// 4 check to see if user input key letter matches letter in answer word
+
+// 5a if step 4 returns true, update place holder text with letter
+
+// 5b if step 4 returns false, update wrong guessbox with with userinput letter guess
+// loop
+
+// 6a if worng guess box contains 10 letters retun with alert sorry please try again- 7a
+
+// 6b if all input key letters match answer word return with alert congratulations!! please continue to the next word
+
+//  7a reset go through steps to whip out board for starting new variable answer word
+
+// 7b start at 3a with new loop
